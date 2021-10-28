ba8c24f068e098f1c355673ea9cab679a787a59e
ridafarrukh <ridafarrukh92@gmail.com>
2021-10-28 14:09:45 +0200

    secong commit

---
+++ frontend-tests/cypress/integration/1-getting-started/regression-test-suite.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+//This is a test suite
+describe("Regression test suite", function(){
+
+    //This is a test case
+    it("Perform valid login", function(){
+        cy.visit("http://localhost:3000")
+       
+
+    })
+
+
+
+
+})
